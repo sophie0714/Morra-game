988d3b313fb842490925eda57291aeef6aedf26b
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-05-16 14:41:50 +1200

    Print error message for invalid inputs from the human player

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -4,6 +4,8 @@ import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
 
+  int numOfRound = 1;
+
   public Morra() {}
 
   public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
@@ -11,7 +13,34 @@ public class Morra {
     MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    // Initiate a game
+    MessageCli.START_ROUND.printMessage(Integer.toString(numOfRound));
+    
+    // Error message for invalid input
+    String input = "";
+    while (!inputValidityChecker(input)){
+      MessageCli.ASK_INPUT.printMessage();
+      input = Utils.scanner.nextLine();
+    }
+    
+  }
 
   public void showStats() {}
+
+  private boolean inputValidityChecker(String input) {
+    String inputs[] = input.split(" ");
+    if (inputs.length != 2) {
+      return false;
+    } else if (!Utils.isInteger(inputs[0]) || !Utils.isInteger(inputs[1])) {
+      return false;
+    } else if (Integer.valueOf(inputs[0]) > 5
+        || Integer.valueOf(inputs[1]) > 10
+        || Integer.valueOf(inputs[0]) == 0
+        || Integer.valueOf(inputs[1]) == 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
