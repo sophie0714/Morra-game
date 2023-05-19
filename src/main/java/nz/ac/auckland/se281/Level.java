6c4a432a504dc16d55b5fa020972e74a0058e844
sophie0714 <jpar875@aucklanduni.ac.nz>
2023-05-19 15:52:42 +1200

    Change the Level interface to abstract class and add setStratege method and giveNumbers method. Also change the return type of changeStrategy from string to void

--- src/main/java/nz/ac/auckland/se281/Level.java
+++ src/main/java/nz/ac/auckland/se281/Level.java
@@ -1,5 +1,20 @@
 package nz.ac.auckland.se281;
 
-public interface Level {
-  String useStrategy();
+public abstract class Level {
+
+  Strategy strategy;
+
+  public Level(Strategy strategy){
+    this.strategy = strategy;
+  }
+
+  abstract void useStrategy();
+
+  public void setStrategy(Strategy strategy){
+    this.strategy = strategy;
+  }
+
+  public String giveNumbers(){
+    return strategy.getNumbers();
+  }
 }
